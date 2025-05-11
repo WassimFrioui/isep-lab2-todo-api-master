201bea1f96b80fa97b13ee2ac68916fd061efa71
Wassim Frioui <wfrioui01@gmail.com>
2025-05-11 12:08:47 +0200

    Step 1 (todo : need to fix the dueDate, actually it's a String and not LocalDate due to a error )

--- src/main/java/org/isep/cleancode/Todo.java
+++ src/main/java/org/isep/cleancode/Todo.java
@@ -1,10 +1,13 @@
 package org.isep.cleancode;
 
+// import java.time.LocalDate;
+
 public class Todo {
 
     // this Todo class should be completed to achieve Step 1
 
     private String name;
+    private String dueDate; // I out this as String because i have a problem with LocalDate
 
     public Todo(String name) {
         this.name = name;
@@ -17,4 +20,13 @@ public class Todo {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
 }
--- src/main/java/org/isep/cleancode/TodoController.java
+++ src/main/java/org/isep/cleancode/TodoController.java
@@ -23,6 +23,26 @@ public class TodoController {
     public Object createTodo(Request req, Response res) {
         Todo newTodo = gson.fromJson(req.body(), Todo.class);
 
+        if (newTodo.getName() == null || newTodo.getName().isEmpty() || newTodo.getName().length() > 64) {
+            res.status(400);
+            return "Todo name is required and should not exceed 64 characters";
+        }
+
+        // dueDate String
+        if (newTodo.getDueDate() == null || newTodo.getDueDate().isEmpty()) {
+            res.status(400);
+            return "Due date is required";
+        }
+
+
+        for (Todo todo : todos) {
+            if (todo.getName().equals(newTodo.getName())) {
+                res.status(400);
+                return "Todo with this name already exists";
+            }
+        }
+
+
         todos.add(newTodo);
         res.status(201);
         res.type("application/json");
